fe4092f73e00e0ac2714ede9c4ceaaba1795d882
Santosh <santosh.d@tekyslab.com>
2019-01-18 11:33:26 +0530

    initial commit for leave apply screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 const routes: Routes = [
     { path: "", redirectTo: "/login", pathMatch: "full" },
-    {path: "login", loadChildren: "~/app/login/login.module#LoginModule"},
+    { path: "login", loadChildren: "~/app/login/login.module#LoginModule" },
     { path: "home", loadChildren: "~/app/home/home.module#HomeModule" },
     { path: "browse", loadChildren: "~/app/browse/browse.module#BrowseModule" },
     { path: "search", loadChildren: "~/app/search/search.module#SearchModule" },
     { path: "featured", loadChildren: "~/app/featured/featured.module#FeaturedModule" },
-    { path: "settings", loadChildren: "~/app/settings/settings.module#SettingsModule" }
+    { path: "settings", loadChildren: "~/app/settings/settings.module#SettingsModule" },
+    { path: "applyleave", loadChildren: "~/app/apply-leave/apply-leave.module#ApplyLeaveModule" }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import {NativeScriptFormsModule} from "nativescript-angular/forms";
 import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { ModalDatetimepicker } from "nativescript-modal-datetimepicker";
 
 @NgModule({
     bootstrap: [
@@ -13,11 +15,15 @@ import { AppComponent } from "./app.component";
     imports: [
         AppRoutingModule,
         NativeScriptModule,
+        NativeScriptFormsModule,
         NativeScriptUISideDrawerModule,
         HttpClientModule
     ],
     declarations: [
         AppComponent
+    ],
+    providers:[
+        ModalDatetimepicker
     ],
     schemas: [
         NO_ERRORS_SCHEMA
